a962b2ea43d812ce4a9ea21f2fb96da5d32c4356
MARIA ISABEL SANTAMARIA URIBE <isabel.santamaria@est.iudigital.edu.co>
2026-03-31 23:22:21 -0500

    Create abstract Empleado class with properties and methods

--- src/main/java/com/miempresa/compuworksystem/model/Empleado.java
+++ src/main/java/com/miempresa/compuworksystem/model/Empleado.java
@@ -1,1 +1,32 @@
 
+package com.miempresa.compuworksystem.model;
+public abstract class Empleado {
+    private int idEmpleado;
+    private String nombre;
+    private String cargo;
+
+    public Empleado(int idEmpleado, String nombre, String cargo) {
+        this.idEmpleado = idEmpleado;
+        this.nombre = nombre;
+        this.cargo = cargo;
+    }
+
+    public abstract double calcularDesempeño();
+
+    public String obtenerDatos() {
+        return "ID: " + idEmpleado + " | Nombre: " + nombre + " | Cargo: " + cargo;
+    }
+
+    
+    public int getIdEmpleado() { return idEmpleado; }
+    public String getNombre() { return nombre; }
+    public String getCargo() { return cargo; }
+
+   
+    public void setNombre(String nombre) { this.nombre = nombre; }
+    public void setCargo(String cargo) { this.cargo = cargo; }
+
+    Object getId() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+}
