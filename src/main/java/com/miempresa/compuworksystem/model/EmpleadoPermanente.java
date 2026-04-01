45d1e021f43bf520fc71a9e3abafba373b2eff73
MARIA ISABEL SANTAMARIA URIBE <isabel.santamaria@est.iudigital.edu.co>
2026-03-31 23:33:52 -0500

    Add EmpleadoPermanente class with salary and bonus

--- src/main/java/com/miempresa/compuworksystem/model/EmpleadoPermanente.java
+++ src/main/java/com/miempresa/compuworksystem/model/EmpleadoPermanente.java
@@ -1,1 +1,30 @@
+package com.miempresa.compuworksystem.model;
+
+import com.miempresa.compuworksystem.model.Empleado;
+
+
+public class EmpleadoPermanente extends Empleado {
+    private double salarioBase;
+    private double bonificacion;
+    private int antiguedad;
+
+    // CONSTRUCTOR
+    public EmpleadoPermanente(int id, String nom, String car, double salario, double bono, int ant) {
+        super(id, nom, car);
+        this.salarioBase = salario;
+        this.bonificacion = bono;
+        this.antiguedad = ant;
+    }
+
+    @Override
+    public double calcularDesempeño() {
+       
+ 
+        return Math.min(10.0, 5.0 + (antiguedad * 0.5));
+    }
+
+    public double calcularBonificacion() {
+        return salarioBase + bonificacion;
+    }
+}
 
