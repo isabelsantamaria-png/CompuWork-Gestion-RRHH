b88ccf5ae30614f17118f330e3456f05d79d9c0e
MARIA ISABEL SANTAMARIA URIBE <isabel.santamaria@est.iudigital.edu.co>
2026-03-31 23:20:16 -0500

    Initialize main class and display main screen

--- src/main/java/com/miempresa/compuworksystem/Main.java
+++ src/main/java/com/miempresa/compuworksystem/Main.java
@@ -1,1 +1,12 @@
+package com.miempresa.compuworksystem;
+
+import com.miempresa.compuworksystem.view.PantallaPrincipal;
+
+public class Main {
+    public static void main(String[] args) {
+
+        PantallaPrincipal pantalla = new PantallaPrincipal();
+        pantalla.setVisible(true);
+    }
+}
 
