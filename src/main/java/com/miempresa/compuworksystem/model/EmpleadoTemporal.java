1ff7fa1a93d41f3651584648ba00b2d51941478b
MARIA ISABEL SANTAMARIA URIBE <isabel.santamaria@est.iudigital.edu.co>
2026-03-31 23:35:47 -0500

    Update EmpleadoTemporal.java

--- src/main/java/com/miempresa/compuworksystem/model/EmpleadoTemporal.java
+++ src/main/java/com/miempresa/compuworksystem/model/EmpleadoTemporal.java
@@ -1,1 +1,23 @@
+package com.miempresa.compuworksystem.model;
+
+import com.miempresa.compuworksystem.model.Empleado;
+import java.util.Date;
+
+public class EmpleadoTemporal extends Empleado {
+    private Date fechaInicio;
+    private Date fechaFin;
+    private int horasTrabajadas;
+
+    // CONSTRUCTOR
+    public EmpleadoTemporal(int id, String nom, String car, int horas) {
+        super(id, nom, car);
+        this.horasTrabajadas = horas;
+        this.fechaInicio = new Date(); 
+    }
+
+    @Override
+    public double calcularDesempeño() {
+        // Cumplimiento de horas
+        return horasTrabajadas >= 160 ? 10.0 : 7.0;
+    }
 
